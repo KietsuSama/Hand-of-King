cb6d5f9b791e2f46cbfb4aa8f6f04eeb0d59f5f1
KietsuSama <32710452+KietsuSama@users.noreply.github.com>
2017-11-17 11:35:16 +0100

    Add files via upload

--- projet java s3/Hand-of-king-master/src/View.java
+++ projet java s3/Hand-of-king-master/src/View.java
@@ -1,18 +1,16 @@
-package src; /**
- * Created by Lucas on 30/10/2017.
- */
-import src.Carte;
-import src.ControlButton;
-
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
 
 public class View extends JFrame {
+
+       JButton cimetierre = new JButton();
+       JButton cimetierre1 = new JButton();
        JButton[] plateau;
-       ArrayList<Carte> tous =new ArrayList<Carte>();
-       ControlButton cb= new ControlButton(this);
+       ControlButton cb = new ControlButton(this);
+       ArrayList<Carte> tous = new ArrayList<Carte>();
+
 
 
        public View(Carte[]cartes){
@@ -32,30 +30,44 @@ public class View extends JFrame {
               plateau = new JButton[36];
 
               for (int i =0 ; i<36; i++){
-                  plateau[i]= new JButton();
-                  plateau[i].setSize(20,20);
-
+                     plateau[i]= new JButton();
+                     plateau[i].setPreferredSize(new Dimension(100,100));
               }
               for(int i=0;i<36;i++) {
-                  plateau[i].setBackground(Color.white);
+                     plateau[i].setBackground(Color.white);
               }
+
+              setVisible(true);
+
        }
        public void initPlateau(Carte[] cartes){
-           for (int i = 0;i<36;i++ ){
-               plateau[i].setIcon(cartes[i].getIcone());
-
-
-
-           }
+              for (int i = 0;i<36;i++ ){
+                     plateau[i].setIcon(cartes[i].getIcone());
+              }
        }
 
        public void creerPaneau(){
-              JPanel jeu= new JPanel(new GridLayout(6,6));
+
+              JPanel pan = new JPanel();
+              JPanel pan1 = new JPanel();
+              JPanel pan2 = new JPanel();
+              JPanel pan3 = new JPanel();
+              pan1.setLayout(new GridLayout(6,6));
+
+
               for(int i=0;i<36;i++){
-                     jeu.add(plateau[i]);
+                     pan1.add(plateau[i]);
                      plateau[i].addActionListener(cb);
               }
-              setContentPane(jeu);
+              cimetierre.setIcon(new ImageIcon("pokémon/plateau.png"));
+              cimetierre1.setIcon(new ImageIcon("pokémon/plateau.png"));
+              pan2.add(cimetierre);
+              pan3.add(cimetierre1);
+              pan.add(pan3);
+              pan.add(pan1);
+              pan.add(pan2);
+              setContentPane(pan);
+              setVisible(true);
        }
 
 
